bb7b6d70620875e67350bf51fac5bb2f96ff6763
mdsalauddin96 <salauddincse96@gmail.com>
2026-05-10 23:19:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express=require('express')
+const cors=require('cors')
+const dotenv=require('dotenv')
+dotenv.config()
+const app=express()
+const port=process.env.PORT;
+app.use(cors())
+app.use(express())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri=process.env.MONGO_URI;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+ 
+async function  run() {
+    try{
+        await client.connect()
+        const db=client.db("wanderlastDB")
+        const destinationColl=db.collection("destinations")
+        app.get("/featuredDestination", async(req,res)=>{
+            const result=await destinationColl.find().limit(5).toArray()
+            res.json(result)
+        })
+        await client.db("admin").command({ping: 1})
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+        // await client.close()
+    }
+}
+run().catch(console.dir);
+
+app.listen(port,()=>{
+    console.log(`wanderlast-server listening on port ${port}`)
+})
