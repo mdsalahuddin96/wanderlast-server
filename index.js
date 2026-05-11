e2269c50da07fbf78ac084e4e60350f96db4ca3e
mdsalahuddin96 <salauddincse96@gmail.com>
2026-05-11 17:49:44 +0600

    make api for get featured destinations

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-const express=require('express')
-const cors=require('cors')
-const dotenv=require('dotenv')
-dotenv.config()
-const app=express()
-const port=process.env.PORT;
-app.use(cors())
-app.use(express())
+//  password=YwtDd5VU33gpwWRw
+// user=wanderlastUser
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri=process.env.MONGO_URI;
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URI;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
- 
-async function  run() {
-    try{
-        await client.connect()
-        const db=client.db("wanderlastDB")
-        const destinationColl=db.collection("destinations")
-        app.get("/featuredDestination", async(req,res)=>{
-            const result=await destinationColl.find().limit(5).toArray()
-            res.json(result)
-        })
-        await client.db("admin").command({ping: 1})
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    finally{
-        // await client.close()
-    }
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("wanderlastDB");
+    const destinationColl = db.collection("destinations");
+    app.get("/featuredDes", async (req, res) => {
+      const result = await destinationColl.find().limit(5).toArray();
+      res.json(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close()
+  }
 }
 run().catch(console.dir);
 
-app.listen(port,()=>{
-    console.log(`wanderlast-server listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Wanderlast-server listening on port ${port}`);
+});
